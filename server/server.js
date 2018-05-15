3c18804c3b8ec60616a599e66111024decd4256f
Martin MacDonald <martinmacdonald@swarmonline.com>
2018-05-15 17:14:23 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }).catch(err => {
+    res.status(400).send(err);
+  })
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.send({
+        todos,
+      });
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        res.status(404).send({});
+      } else {
+        res.send({ todo });
+      }
+    }).catch((err) => {
+      res.status(400).send({});
+    });
+});
+
+app.listen(3000, () => {
+  console.log('started on port 3000');
+});
+
+module.exports = {
+  app
+}
